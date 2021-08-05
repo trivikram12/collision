6eff9cafe4427cc2fc9f6e17edb4cd155f9bc1ed
trivikram12 <87323735+trivikram12@users.noreply.github.com>
2021-08-05 20:01:25 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+var fixedRect, movingRect;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(600, 400, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  rect1 = createSprite(200, 400, 50, 80);
+  rect1.shapeColor = "green";
+  rect1.debug = true;
+  movingRect = createSprite(400,200,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+}
+
+function draw() {
+  background(0,0,0);  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+if (isTouching(rect1,movingRect)){
+  movingRect.shapeColor = "blue";
+  rect1.shapeColor = " blue";
+  
+
+} else{
+  movingRect.shapeColor = "green";
+  rect1.shapeColor = "green";
+}
+  
+  drawSprites();
+}
